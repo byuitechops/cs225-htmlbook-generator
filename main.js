1a8c8a6561b7b1889bfdfadae27ede043f4ab7f1
Wilson <kilakal@byui.edu>
2018-12-12 14:37:34 -0700

    finsihed replace code blocks.

--- main.js
+++ main.js
@@ -2,10 +2,29 @@
  * Module Description
  *************************************************************************/
 const tasks = require('./tasks/tasks.js');
+const fs = require('fs');
+
+function readFile(callback) {
+    fs.readFile('./html_files/index.html', 'utf-8', (err, html) => {
+        if (err) {
+            callback(err, null);
+        }
+        callback(null, html);
+    });
+}
 
 module.exports = {
     main(args) {
-        tasks.replaceCodeBlocks();
+        readFile((err, html) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            tasks.replaceCodeBlocks(html)
+                .then(console.log)
+                .catch(console.error);
+            console.log(html);
+        });
         return 0;
     }
 };
--- tasks/replaceCodeBlocks.js
+++ tasks/replaceCodeBlocks.js
@@ -1,44 +1,34 @@
-const cheerio = require('cheerio'),
-    fs = require('fs');
-var $;
+const cheerio = require('cheerio');
+const fs = require('fs');
+let $;
 
-function editBlock() {
-    let wpSyntaxClasses = $('.wp_syntax');
-    wpSyntaxClasses.each((i, element) => {
-        let newElement = $(element)
-        let actualCode = newElement.find('.theCode').text();
-        let language = newElement.find('pre').attr('class');
-        let languageMap = {
-            sql: ' class="langauge-sql"',
-            oracle11: ' class="langauge-sql"',
-            mysql: ' class="langauge-sql"',
-            oracle11i: ' class="langauge-sql"',
-            plsql: ' class="langauge-plsql"',
-            bash: ' class="langauge-bash"',
-            dos: ' class="langauge-powershell"',
-            php: ' class="langauge-php"',
-            html: ' class="langauge-html"',
-            java: ' class="langauge-java"'
-
-            //NOTE: for .txt, .text, .test, and any other mistyped classes that may arise
-            //[`${}`]: ''
-        }
-        newElement.replaceWith(`<pre><code${languageMap[language]}>${actualCode}</code></pre>`);
-    });
-    console.log($.html());
-}
-
-function replaceCodeBlocks() {
-    fs.readFile('./html_files/index.html', 'utf-8', (err, html) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
+function replaceCodeBlocks(html) {
+    return new Promise((resolve, reject) => {
         $ = cheerio.load(html);
-        editBlock();
+        let wpSyntaxClasses = $('.wp_syntax');
+        wpSyntaxClasses.each((i, element) => {
+            let newElement = $(element);
+            let actualCode = newElement.find('.theCode').text();
+            let language = newElement.find('pre').attr('class');
+            let languageMap = {
+                sql: 'sql',
+                oracle11: 'sql',
+                mysql: 'sql',
+                oracle11i: 'sql',
+                plsql: 'plsql',
+                bash: 'bash',
+                dos: 'powershell',
+                php: 'php',
+                html: 'html',
+                java: 'java'
+            };
+            if (languageMap[language]) {
+                newElement.replaceWith(`<pre><code class="language-${languageMap[language]}">${actualCode}</code></pre>`);
+            } else {
+                newElement.replaceWith(`<pre><code class="language-markup">${actualCode}</code></pre>`);
+            }
+        });
+        resolve($.html());
     });
-
 }
-
-
 module.exports = replaceCodeBlocks;
