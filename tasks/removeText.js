9ea94b6795a00adff978189924e6bddc7fd5f896
Wilson <kilakal@byui.edu>
2019-01-25 10:55:00 -0700

    Added new tasks. Fixed template

--- tasks/removeText.js
+++ tasks/removeText.js
@@ -2,8 +2,15 @@ const cheerio = require('cheerio');
 let $;
 
 function removeText(htmlObj, callback) {
-    console.log(htmlObj);
     $ = cheerio.load(htmlObj.htmlText);
+    let textToRemove = [$('.comments'), $('.signature'), $('.tags'), $('.dropdownclick')];
+    textToRemove.forEach(item => {
+        item.each(function () {
+            let element = $(this);
+            element.replaceWith('');
+        });
+    });
+    htmlObj.htmlText = $.html();
     callback(null, htmlObj);
 }
 
--- tasks/tasks.js
+++ tasks/tasks.js
@@ -1,4 +1,7 @@
 let removeText = require('./removeText.js');
+let removeUnnecessaryPTags = require('./removeUnnecessaryPTags.js');
+let fixULTags = require('./fixULTags.js');
+let fixTitle = require('./fixTitle.js');
 let fixDropdowns = require('./fixDropdowns');
 let replaceCodeBlocks = require('./replaceCodeBlocks.js');
 
@@ -6,6 +9,9 @@ module.exports = function (htmlObj) {
     return [
         (callback) => callback(null, htmlObj),
         removeText,
+        removeUnnecessaryPTags,
+        fixULTags,
+        fixTitle,
         fixDropdowns,
         replaceCodeBlocks
     ];
